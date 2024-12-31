f336b00a6255b4de84817964d6a952934119d244
katatrina <cvphuoc2014@gmail.com>
2025-01-01 03:20:38 +0700

    minor update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import './assets/main.css' // global css
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue' // imported function APIs
+import App from './App.vue' // import root component
+import router from './router' // import router
 
-const app = createApp(App)
+const app = createApp(App) // create application instance
 
-app.use(router)
+app.use(router) // register router
 
-app.mount('#app')
+app.mount('#app') // call mount() to render UI, it return the root component "App"
