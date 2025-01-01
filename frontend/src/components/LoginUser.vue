14c5321930a939c977e6c4913118faa40f7b1e16
katatrina <cvphuoc2014@gmail.com>
2025-01-01 11:04:49 +0700

    build login form with PrimeVue and Tailwind CSS

---
+++ frontend/src/components/LoginUser.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputText from 'primevue/inputtext';
+
+</script>
+
+<template>
+  <Form class="flex flex-col gap-4">
+    <div>
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-user"></i>
+        </InputGroupAddon>
+        <InputText name="username" type="text" placeholder="Username" class="rounded-r-md" />
+      </InputGroup>
+    </div>
+
+    <div>
+      <InputGroup>
+        <InputGroupAddon><i class="pi pi-lock"></i></InputGroupAddon>
+        <InputText name="password" type="password" placeholder="Password" />
+      </InputGroup>
+    </div>
+  </Form>
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,21 @@ import './assets/main.css' // global css
 import { createApp } from 'vue' // imported function APIs
 import App from './App.vue' // import root component
 import router from './router' // import router
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App) // create application instance
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+})
 
 app.use(router) // register router
 
